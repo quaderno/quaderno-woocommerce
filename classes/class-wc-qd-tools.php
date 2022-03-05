c1749470f8e9ed83831b5bb36d177ef84c94be18
Carlos Hernandez <carlos@quaderno.io>
2022-03-05 14:09:50 +0000

    Add tool to delete Quaderno data

--- classes/class-wc-qd-tools.php
+++ classes/class-wc-qd-tools.php
@@ -4,28 +4,46 @@
   }
 
   class WC_QD_Tools {
-    function __construct() {
+    public function __construct() {
       add_filter( 'woocommerce_debug_tools', array( $this,'qd_cache_cleaning_button' ) );
+      add_filter( 'woocommerce_debug_tools', array( $this,'qd_metadata_cleaning_button' ) );
     }
   
-    function qd_cache_cleaning_button( $old ) {
-        $new = array(
-            'clear_tax_cache' => array(
-                'name'    => __( 'Clear tax cache', 'woocommerce-quaderno' ),
-                'button'  => __( 'Clear', 'woocommerce' ),
-                'desc'    => sprintf(
-                  '<strong class="red">%1$s</strong> %2$s',
-                  __( 'Note:', 'woocommerce' ),
-                  __( 'This tool will empty the tax cache created by Quaderno.', 'woocommerce-quaderno' )
-                ),
-                'callback'  => array( $this, 'qd_cache_cleaning_action' ),
-            ),
-        );
-        $tools = array_merge( $old, $new );
-        return $tools;
+    public function qd_cache_cleaning_button( $old ) {
+      $new = array(
+          'clear_tax_cache' => array(
+              'name'    => __( 'Clear tax cache', 'woocommerce-quaderno' ),
+              'button'  => __( 'Clear', 'woocommerce' ),
+              'desc'    => sprintf(
+                '<strong class="red">%1$s</strong> %2$s',
+                __( 'Note:', 'woocommerce' ),
+                __( 'This tool will empty the tax cache created by Quaderno.', 'woocommerce-quaderno' )
+              ),
+              'callback'  => array( $this, 'qd_cache_cleaning_action' ),
+          ),
+      );
+      $tools = array_merge( $old, $new );
+      return $tools;
+    }
+
+    public function qd_metadata_cleaning_button( $old ) {
+      $new = array(
+          'clear_metadata' => array(
+              'name'    => __( 'Clear Quaderno data', 'woocommerce-quaderno' ),
+              'button'  => __( 'Clear', 'woocommerce' ),
+              'desc'    => sprintf(
+                '<strong class="red">%1$s</strong> %2$s',
+                __( 'Note:', 'woocommerce' ),
+                __( 'This tool will clear all data created by Quaderno. Use this if you are going to connect your WooCommerce store to a new Quaderno account.', 'woocommerce-quaderno' )
+              ),
+              'callback'  => array( $this, 'qd_metadata_cleaning_action' ),
+          ),
+      );
+      $tools = array_merge( $old, $new );
+      return $tools;
     }
     
-    function qd_cache_cleaning_action() {
+    public function qd_cache_cleaning_action() {
       global $wpdb;
 
       $sql = 'DELETE FROM ' . $wpdb->options . ' WHERE option_name LIKE "_transient_quaderno_tax_%"';
@@ -35,5 +53,18 @@
       $wpdb->query($sql);
 
       echo '<div class="updated"><p>' . __( 'The tax cache has been emptied.', 'woocommerce-quaderno' ) . '</p></div>';
+    }
+
+    public function qd_metadata_cleaning_action()
+    {
+      global $wpdb;
+
+      $sql = 'DELETE FROM ' . $wpdb->postmeta . ' WHERE meta_key LIKE "_quaderno_%"';
+      $wpdb->query($sql);
+
+      $sql = 'DELETE FROM ' . $wpdb->usermeta . ' WHERE meta_key LIKE "_quaderno_%"';
+      $wpdb->query($sql);
+
+      echo '<div class="updated"><p>' . __( 'All Quaderno data has been deleted.', 'woocommerce-quaderno' ) . '</p></div>';
     }
 }
