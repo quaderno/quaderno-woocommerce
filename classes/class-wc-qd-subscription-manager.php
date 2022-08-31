e985ad4c11052bc5e60294a5cf89544224f1b09e
Carlos Hernandez <carlos@quaderno.io>
2022-08-31 13:25:13 +0100

    Fix: we cannot recalculate subtotal for shipping item in subscriptions

--- classes/class-wc-qd-subscription-manager.php
+++ classes/class-wc-qd-subscription-manager.php
@@ -36,12 +36,15 @@ class WC_QD_Subscription_Manager extends WC_QD_Transaction_Manager {
     $items = $new_order->get_items(array('line_item', 'shipping' ,'fee'));
 
     foreach ( $items as $item_id => $item ) {
-      $subtotal = $new_order->get_line_subtotal( $item, true );
-      $total = $new_order->get_line_total( $item, true );
       $tax = $this->get_tax( $new_order, $item );
 
       // Recalculate item's subtotal and total
-      $item->set_subtotal( $subtotal + round( $subtotal * $tax->rate / 100, 2 ));
+      if ( $item->is_type( 'line_item' ) ) {
+        $subtotal = $new_order->get_line_subtotal( $item, true );
+        $item->set_subtotal( $subtotal + round( $subtotal * $tax->rate / 100, 2 ));
+      }
+
+      $total = $new_order->get_line_total( $item, true );
       $item->set_total( $total + round( $total * $tax->rate / 100, 2 ));
 
       $item->calculate_taxes(); // Make new taxes calculations
--- readme.txt
+++ readme.txt
@@ -4,7 +4,7 @@ Contributors: polimorfico
 Tags: tax, taxes, sales tax, vat, gst, vatmoss, vat moss, billing, invoices, receipts, credit notes, woocommerce, quaderno
 Requires at least: 4.6
 Tested up to: 6.0
-Stable tag: 2.1.8
+Stable tag: 2.1.9
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
@@ -66,6 +66,9 @@ If you have any questions please get in touch with us at support@quaderno.io.
 4. Example of a receipt
 
 == Changelog ==
+
+= 2.1.9 – August 31, 2022 =
+* Fix: we cannot recalculate subtotal for shipping item in subscriptions
 
 = 2.1.8 – July 11, 2022 =
 * Fix: tax ID is not saved in programatically created orders
